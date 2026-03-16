71c67eec7d789e1765d9993edd2aba9f69c06644
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-03-16 11:15:07 +0000

    feat: initialisation es api et creation API pour le test

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(
+        [
+            'message' => "Welcome to API"
+        ]
+    );
 });
