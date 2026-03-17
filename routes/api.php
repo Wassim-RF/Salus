91815fb18c90dd2ee834fd915c135a266c7ba5a9
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-03-17 09:27:29 +0000

    feat: ajoute LogoutController avec son logique

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,5 +10,11 @@ Route::get('/test' , function() {
     return response()->json();
 });
 
-Route::post('/register' , [RegisterController::class , 'register']);
-Route::post('/login' , [LoginController::class , 'login']);
+// Auth
+Route::post('register' , [RegisterController::class , 'register']);
+Route::post('login' , [LoginController::class , 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('logout' , [LogoutController::class , 'logout']);
+});
