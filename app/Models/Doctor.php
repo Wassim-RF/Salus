13b0d720016a87e1970339f7753d7156fe681abd
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-03-19 22:08:18 +0000

    feat: ajoute fillable et casts au model Doctor

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'speciality',
+        'city',
+        'years_of_experience',
+        'consultation_price',
+        'available_days'
+    ];
+
+    protected $casts = [
+        'available_days' => 'array'
+    ];
 }
